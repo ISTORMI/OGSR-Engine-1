75aa8e2a3baf108dd09c7d0bd0910e14515bd4c7
Username3228 <75955587+ISTORMI@users.noreply.github.com>
2023-06-21 16:29:30 +0300

    rpc?

--- ogsr_engine/xr_3da/Device_Initialize.cpp
+++ ogsr_engine/xr_3da/Device_Initialize.cpp
@@ -65,7 +65,7 @@ void CRenderDevice::Initialize()
 
         // Create the render window
         m_hWnd = CreateWindow /*Ex*/ ( // WS_EX_TOPMOST,
-            wndclass, "S.T.A.L.K.E.R.: Call of Pripyat", m_dwWindowStyle,
+            wndclass, "dsh mod", m_dwWindowStyle,
             /*rc.left, rc.top, */ CW_USEDEFAULT, CW_USEDEFAULT, (rc.right - rc.left), (rc.bottom - rc.top), 0L, 0, hInstance, 0L);
         gGameWindow = m_hWnd;
     }
